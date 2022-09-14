536b35a54cf809989133cc725467ce95a0011fc2
arunnachalam <arunr01499@gmail.com>
2022-09-14 09:04:38 +0600

    first commit

---
+++ src/com/net/N.java
@@ -0,0 +1,22 @@
+package com.net;
+
+public  class N {
+	
+	
+	
+	private static void Java(int...a) {
+		
+		for (int i = 0; i < a.length; i++) {
+			
+			{
+				System.out.println(a[i]);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		N n = new N();
+		n.Java(1,2,3,4,5,6,7,8,9);
+	}
+
+}
